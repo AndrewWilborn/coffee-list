1a9468bb91456c39a32e664db89adbf9cbe69095
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-06-29 14:45:29 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Coffees from './components/Coffees';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Coffees />
+      <Footer />
     </>
   );
 }
